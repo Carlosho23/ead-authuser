9c895a57a712ec0efc07c91338a2fc8001f9ecd7
Carlosho23 <carlosho23@hotmail.com>
2025-06-16 22:44:09 -0300

    Implementa validação de usuário

--- src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -2,11 +2,13 @@ package com.ead.authuser.controllers;
 
 import com.ead.authuser.dtos.UserRecordDto;
 import com.ead.authuser.services.UserService;
+import com.ead.authuser.validations.UserValidator;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,17 +20,16 @@ public class AuthenticationController {
 
     final UserService userService;
 
+    final UserValidator userValidator;
+
     @PostMapping("/signup")
     public ResponseEntity<Object> registerUser(@RequestBody @Validated(UserRecordDto.UserView.RegistrationPost.class)
-                                               @JsonView(UserRecordDto.UserView.RegistrationPost.class) UserRecordDto userRecordDto) {
+                                               @JsonView(UserRecordDto.UserView.RegistrationPost.class) UserRecordDto userRecordDto,
+                                               Errors errors) {
         log.debug("POST registerUser UserRecordDto received: {}", userRecordDto);
-        if (userService.existsByUsername(userRecordDto.username())) {
-            log.warn("Username {} is Already Taken ", userRecordDto.username());
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Username is already in Taken!");
-        }
-        if (userService.existsByEmail(userRecordDto.email())) {
-            log.warn("Email {} is Already Taken ", userRecordDto.email());
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Email is already in Taken!");
+        userValidator.validate(userRecordDto, errors);
+        if (errors.hasErrors()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors.getAllErrors());
         }
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.registerUser(userRecordDto));
     }
