e64610bd2c009d6dc250035132d47876a9191e71
Carlosho23 <carlosho23@hotmail.com>
2025-07-10 00:41:48 -0300

    feat: Implementa Resilience4j para CourseClient

--- src/main/java/com/ead/authuser/clients/CourseClient.java
+++ src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -2,15 +2,19 @@ package com.ead.authuser.clients;
 
 import com.ead.authuser.dtos.CourseRecordDto;
 import com.ead.authuser.dtos.ResponsePageDto;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientException;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Component
@@ -26,6 +30,7 @@ public class CourseClient {
         this.restClient = restClientBuilder.build();
     }
 
+    @Retry(name = "retryInstance", fallbackMethod = "retryfallback")
     public Page<CourseRecordDto> getAllCoursesByUser(UUID userId, Pageable pageable) {
         String url = baseUrlCourse + "/courses?userId=" + userId + "&page=" +
                 pageable.getPageNumber() + "&size=" + pageable.getPageSize() +
@@ -43,6 +48,15 @@ public class CourseClient {
             log.error("Error Request RestClient with couse: {} ", e.getMessage());
             throw new RuntimeException("Error Request RestClient: ", e);
         }
+    }
+
+    /**
+     * Caso falha a busca de cursos ele tem um método alternativo para fazer uma prevenção na perda de dados ou retorno
+     */
+    public Page<CourseRecordDto> retryfallback(UUID userId, Pageable pageable, Throwable throwable) {
+        log.error("Inside retryfallback, cause - {} ", throwable.getMessage());
+        List<CourseRecordDto> courseRecordDtoList = new ArrayList<>();
+        return new PageImpl<>(courseRecordDtoList);
     }
 
 }
--- src/main/java/com/ead/authuser/configs/RestClientConfig.java
+++ src/main/java/com/ead/authuser/configs/RestClientConfig.java
@@ -1,16 +1,32 @@
 package com.ead.authuser.configs;
 
+import org.springframework.boot.http.client.ClientHttpRequestFactoryBuilder;
+import org.springframework.boot.http.client.ClientHttpRequestFactorySettings;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
 
+    static final int TIMEOUT = 5000;
+
+    @Bean
+    public ClientHttpRequestFactory customRequestFactory(ClientHttpRequestFactoryBuilder<?> builder) {
+        ClientHttpRequestFactorySettings settings = ClientHttpRequestFactorySettings.defaults()
+                .withConnectTimeout(Duration.ofMillis(TIMEOUT))
+                .withReadTimeout(Duration.ofMillis(TIMEOUT));
+
+        return builder.build(settings);
+    }
+
     @LoadBalanced
     @Bean
-    RestClient.Builder restClientBuilder() {
-        return RestClient.builder();
+    public RestClient.Builder restClientBuilder(ClientHttpRequestFactory factory) {
+        return RestClient.builder().requestFactory(factory);
     }
 }
